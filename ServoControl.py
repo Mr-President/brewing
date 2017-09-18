819f561e068705faa3e14e4786599ce0d285e892
Mr-President <zachmassa94@gmail.com>
2017-09-17 20:09:25 -0500

    Add files via upload

---
+++ ServoControl.py
@@ -0,0 +1,16 @@
+import rpi.gpio as gipo
+import time
+
+GPIO.setmode(gpio.board)
+GPIO.setup(8,GPIO.OUT)
+
+p = GPIO.PWM(8,50)
+
+speed = input('Forward or Back?')
+
+if str.lower(speed) == 'f':
+	p.start(10.0)
+elif str.lower(speed) == 'b':
+	p.start(75.0)
+else:
+	print('That is not a valid input, please use f for forward or b for back')
