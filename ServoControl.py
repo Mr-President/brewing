13906cc8334ae1c8536e5479b41b41a06a82ba31
Mr-President <zachmassa94@gmail.com>
2017-09-18 17:57:41 -0500

    Update ServoControl.py

--- ServoControl.py
+++ ServoControl.py
@@ -5,21 +5,5 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8,GPIO.OUT)
 
 p = GPIO.PWM(8,50)
-kg = True
 
-speed = raw_input('Forward or Back?')
-
-while True:
-	if speed.lower() == 'f':
-		p.start(25.0)
-	elif speed.lower() == 'b':
-		p.start(75.0)
-	else:
-		print('This is not a correct command')
-	kg = input('Keep going?')
-	
-	if kg.lower() == 'y':
-		kg = True
-	else:
-		break
-GPIO.cleanup()
+p.start(85.0)
