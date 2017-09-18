b9408de27c13d92f9302adf2c9d031fb13e6201b
Mr-President <zachmassa94@gmail.com>
2017-09-18 18:12:37 -0500

    Update ServoControl.py

--- ServoControl.py
+++ ServoControl.py
@@ -10,7 +10,7 @@ def setangle(angle):
   GPIO.output(03, True)
   pwm.ChangeDutyCycle(duty)
   sleep(1)
-  GPIO.output(03, Flase)
+  GPIO.output(03, False)
   pwm.ChangeDutyCycle(0)
   
 setangle(90)
