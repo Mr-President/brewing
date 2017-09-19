ebce15b2d1f95e19dd9cda7629d5a6cb93007342
Mr-President <zachmassa94@gmail.com>
2017-09-19 18:08:37 -0500

    Update ServoControl.py

--- ServoControl.py
+++ ServoControl.py
@@ -1,13 +1,13 @@
 import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(03, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 pwm = GPIO.PWM(12,50)
 pwm.start(0)
 
 def setangle(angle):
   duty = 75.0
-  GPIO.output(03, True)
+  GPIO.output(12, True)
   pwm.ChangeDutyCycle(duty)
   sleep(5)
   GPIO.output(12, False)
