8d89dbfa9c4ce671f4e3785d76451294fe348a2b
Mr-President <zachmassa94@gmail.com>
2017-09-19 23:06:15 -0500

    Update ServoControl.py

--- ServoControl.py
+++ ServoControl.py
@@ -8,7 +8,7 @@ try:
         GPIO.output(18, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
         sleep(0.5)                 # wait half a second  
         GPIO.output(18, 0)         # set GPIO24 to 0/GPIO.LOW/False  
-        sleep(0.5)                 # wait half a second  
+        sleep(5)                 # wait half a second  
   
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
     GPIO.cleanup()                 # resets all GPIO ports used by this program  
