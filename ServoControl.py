4a887e58b63c1463045fc60893aca85f035099d7
Mr-President <zachmassa94@gmail.com>
2017-09-19 22:26:45 -0500

    Update ServoControl.py

--- ServoControl.py
+++ ServoControl.py
@@ -2,12 +2,13 @@ import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(18,50)
 
 try:
   while True:
     GPIO.output(18, 1)
-    sleep(5)
-    GPIO.output(18, 0)
-    sleep(5)
-except KeyboardInterrupt:
+    pwm.start(0)
+    pwm.ChangeDutyCycle(7.5)
+    sleep(10)    
+dInterrupt:
   GPIO.cleanup()
