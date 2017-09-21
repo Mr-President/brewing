91630233039b006985343bd42fdfd9cf0d68f780
Mr-President <zachmassa94@gmail.com>
2017-09-21 11:17:34 -0500

    Update ServoControl.py

--- ServoControl.py
+++ ServoControl.py
@@ -1,14 +1,10 @@
-import RPi.GPIO as GPIO            # import RPi.GPIO module  
-from time import sleep             # lets us have a delay  
-GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
-GPIO.setup(18, GPIO.OUT)           # set GPIO24 as an output   
-  
-try:  
-    while True:  
-        GPIO.output(18, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
-        sleep(0.5)                 # wait half a second  
-        GPIO.output(18, 0)         # set GPIO24 to 0/GPIO.LOW/False  
-        sleep(5)                 # wait half a second  
-  
-except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
-    GPIO.cleanup()                 # resets all GPIO ports used by this program  
+#! /usr/bin/env python 
+
+import servoblaster
+import time
+servoblaster.initialize([29],100000,0)
+sevoblaster.position(0, 500)
+sleep(2)
+servoblaster.position(0,1000)
+sleep(2)
+servotblaster.stop()
