78665bd6e6fe4f437dface6051be97f4157b6eeb
Mr-President <zachmassa94@gmail.com>
2017-09-22 07:19:31 -0500

    Update ServoControl.py

--- ServoControl.py
+++ ServoControl.py
@@ -3,8 +3,8 @@
 import servoblaster
 import time
 servoblaster.initialize([29],100000,0)
-servoblaster.position(0, 500)
+servoblaster.position(0, 120)
 time.sleep(2)
-servoblaster.position(0,1000)
+servoblaster.position(0,220)
 time.sleep(2)
 servoblaster.stop()
