84181a933dbd158c4db418d9030e9e9cc1164fda
Mr-President <zachmassa94@gmail.com>
2017-10-03 08:30:29 -0500

    Created some of the basics

--- dataman.py
+++ dataman.py
@@ -1,1 +1,12 @@
+# converts incoming data array from temperature measurments
+# The incoming data should be of the form [Temp, Time, Date] this is put into a dictionary
+# The dictionary can then be converted into a comma delimited file at the end of the program
+
+import os
+import time
+import Temp
+
+def get_dat():
+  
+
 
