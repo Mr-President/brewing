884dd0eabf1e7415a16a0ec136d9581ac44ec9f5
Mr-President <zachmassa94@gmail.com>
2018-02-13 20:03:07 -0600

    Update FermController.py

--- FermController.py
+++ FermController.py
@@ -69,7 +69,7 @@ while True:
 	try:
 		td = gettime(lshutoff)
 		temdat = Temp.read_temp(setpoint)
-		curt = int(round(float(temdat[1],0)))
+		curt = int(round(float(temdat[1]),0))
 		if td >= saverate:
 			temdat = ",".join(temdat)
 			with open(datfil,"w+") as f:
