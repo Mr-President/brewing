7270c6b925cf5e65f51c3bc8d0d22c4bf95d9cd4
Mr-President <zachmassa94@gmail.com>
2018-02-16 18:18:15 -0600

    Update FermController.py

--- FermController.py
+++ FermController.py
@@ -74,7 +74,7 @@ try:
 			temdat = ",".join(temdat)
 			with open(datfil,"w+") as f:
 				f.write(temdat)
-		if hstat == "true":
+		elif hstat == "true":
 			if curt > setpoint:
 				if curt - setpoint > overt:
 					setpi(False,pin)
